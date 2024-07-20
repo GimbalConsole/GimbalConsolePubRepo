daa52c56c7c0d241af396b827e441897c6e605c0
gimbaladmin <gimbaltest123@hotmail.com>
2024-07-20 09:36:11 +0800

    add install script for libxcb-cursor0

---
+++ install_qt_default.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install libxcb-cursor0
+sudo apt-get install libxcb1 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 libxcb-xfixes0 libxcb-xinerama0 libxcb-xkb1 libxkbcommon-x11-0
+sudo apt-get install qt5-default
+dpkg -l | grep libxcb
